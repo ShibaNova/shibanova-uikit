6b73feb79aedc801e5ff46919c65c06d9beee2fb
Diego Brito <britodiego.tech@gmail.com>
2021-07-14 20:02:26 -0400

    style: Use nova_token.png as NovaRound Icon

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -413,7 +413,7 @@ var Icon$u = function (props) {
 
 var Icon$v = function (props) {
     return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 90 90" }, props),
-        React__default['default'].createElement("image", { width: 90, height: 90, href: "/images/egg/9.png" })));
+        React__default['default'].createElement("image", { width: 90, height: 90, href: "/images/coins/nova_token.png" })));
 };
 
 var Icon$w = function (props) {
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -400,7 +400,7 @@ var Icon$u = function (props) {
 
 var Icon$v = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 90 90" }, props),
-        React.createElement("image", { width: 90, height: 90, href: "/images/egg/9.png" })));
+        React.createElement("image", { width: 90, height: 90, href: "/images/coins/nova_token.png" })));
 };
 
 var Icon$w = function (props) {
