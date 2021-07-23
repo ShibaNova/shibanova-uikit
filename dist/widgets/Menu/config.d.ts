b64ecf42aa7932294f86632d7e44414c5ce71dc6
n8urm8 <81867790+n8urm8@users.noreply.github.com>
2021-07-23 12:25:24 -0400

    update logo

--- dist/widgets/Menu/config.d.ts
+++ dist/widgets/Menu/config.d.ts
@@ -25,7 +25,7 @@ export declare const more: {
     label: string;
     href: string;
 }[];
-export declare const MENU_HEIGHT = 64;
+export declare const MENU_HEIGHT = 100;
 export declare const MENU_ENTRY_HEIGHT = 48;
 export declare const SIDEBAR_WIDTH_FULL = 240;
 export declare const SIDEBAR_WIDTH_REDUCED = 56;
