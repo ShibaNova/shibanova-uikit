ae10b9ba07625cdf6c533c65bad924c5f31c1576
cristian123-rocketship <cristian123@rocketship.com>
2021-09-04 16:00:01 +0100

    Test.

--- dist/widgets/Menu/config.d.ts
+++ dist/widgets/Menu/config.d.ts
@@ -41,7 +41,7 @@ export declare const more: {
     href: string;
 }[];
 export declare const MENU_HEIGHT = 200;
-export declare const MENU_HEIGHT_MOBILE = 64;
+export declare const MENU_HEIGHT_MOBILE = 80;
 export declare const MENU_ENTRY_HEIGHT = 48;
 export declare const SIDEBAR_WIDTH_FULL = 240;
 export declare const SIDEBAR_WIDTH_REDUCED = 56;
--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -85,7 +85,7 @@ export const more = [
 ];
 
 export const MENU_HEIGHT = 200;
-export const MENU_HEIGHT_MOBILE = 64;
+export const MENU_HEIGHT_MOBILE = 80;
 export const MENU_ENTRY_HEIGHT = 48;
 export const SIDEBAR_WIDTH_FULL = 240;
 export const SIDEBAR_WIDTH_REDUCED = 56;
